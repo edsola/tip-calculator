da53dec814849ede20b64c51441ec94d2b29184e
edsola <ednasolbau@gmail.com>
2025-01-19 19:13:43 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { menuItems } from "./data/db"
+
+function App() {
+
+  return (
+    <>
+      <header className=" bg-teal-400 py-5 ">
+        <h1 className=" text-center text-4xl font-black ">Tip Calculator</h1>
+      </header>
+
+      <main className=" max-w-7xl mx-auto py-20 grid grid-cols-2">
+        <div>
+          <h2>Menu</h2>
+        </div>
+
+
+        <div>
+          <h2>Consumo</h2>
+        </div>        
+      </main>
+    </>
+  )
+}
+
+export default App
