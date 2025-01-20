2736d6a43bb47a40bf51a8c809ce22bed6191c3c
edsola <ednasolbau@gmail.com>
2025-01-20 16:12:40 +0100

    Menu item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import MenuItem from "./components/MenuItem"
 import { menuItems } from "./data/db"
 
 function App() {
@@ -8,10 +9,20 @@ function App() {
         <h1 className=" text-center text-4xl font-black ">Tip Calculator</h1>
       </header>
 
-      <main className=" max-w-7xl mx-auto py-20 grid grid-cols-2">
-        <div>
-          <h2>Menu</h2>
+      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className="p-5">
+          <h2 className="text-3xl font-black">Menu</h2>
+
+          <div className="space-y-3 mt-7">
+            {menuItems.map(item => (
+              <MenuItem
+                key={item.id}
+                item={item}
+              />
+            ))}
+          </div>
         </div>
+
 
 
         <div>
