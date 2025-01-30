64b48542b37d8f88293e0213e1155cc109a99446
edsola <ednasolbau@gmail.com>
2025-01-30 11:34:17 +0100

    placeOrder button

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import useOrder from "./hooks/useOrder"
 
 function App() {
 
-  const { order, addItem, removeItem, tip, setTip } = useOrder()
+  const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder()
 
 
   return (
@@ -33,19 +33,27 @@ function App() {
 
 
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          <OrderContents 
-            order={order}
-            removeItem={removeItem}
-          />
+            { order.length > 0 ? (
+              <>
+                <OrderContents 
+                  order={order}
+                  removeItem={removeItem}
+                />
 
-          <TipPercentageForm 
-            setTip={setTip}
-          />
+                <TipPercentageForm 
+                  setTip={setTip}
+                  tip={tip}
+                />
 
-          <OrderTotals
-            order={order}
-            tip={tip}
-          />
+                <OrderTotals
+                  order={order}
+                  tip={tip}
+                  placeOrder={placeOrder}
+                />
+              </>
+            ) : (
+              <p className="text-center">Orden vacía</p>
+            )}
         </div>        
       </main>
     </>
--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -5,9 +5,10 @@ import { formatCurrency } from "../helpers"
 type OrderTotalsProps = {
     order: OrderItem[]
     tip: number
+    placeOrder: () => void
 }
 
-export default function OrderTotals({order, tip} : OrderTotalsProps)  {
+export default function OrderTotals({order, tip, placeOrder} : OrderTotalsProps)  {
 
     const subtotalAmount = useMemo(() => 
         order.reduce((total, item) => 
@@ -36,7 +37,13 @@ export default function OrderTotals({order, tip} : OrderTotalsProps)  {
                 
             </div>
 
-
+            <button
+                className="w-full bg-black p-3 uppercase text-white font-bold mt-10 disabled:opacity-10"
+                disabled={totalAmount === 0}
+                onClick={placeOrder}
+            >
+                Guardar orden
+            </button>
         </>
     )
 }
